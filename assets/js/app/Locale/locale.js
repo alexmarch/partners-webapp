56cdbf397004eb8b7aa197e19ea1efc8bab08bb7
Alexander Marchenko <maappdev@gmail.com>
2014-12-15 15:47:47 +0200

    Fix locale directive

--- assets/js/app/Locale/locale.js
+++ assets/js/app/Locale/locale.js
@@ -5,10 +5,10 @@ angular.module('app.locale', ['pascalprecht.translate']);
 angular.module('app.locale').directive('appLocale', ['$translate', function($translate) {
   return {
     strict: 'AE',
-    template: '<div class="pull-right"><a href="" ng-click="toggleLanguage()">EN</a><a ng-click="toggleLanguage()">RU</a></div>',
+    template: '<div class="app-locale"><a href ng-click="toggleLanguage()">EN</a><a href ng-click="toggleLanguage()">RU</a></div>',
     link: function($scope, element, attr) {
       $scope.toggleLanguage = function() {
-        $translate.use($translate.use() === 'en' ? 'ru' : 'en');
+        $translate.use(($translate.use() === 'en') ? 'ru' : 'en');
         console.log($translate.use());
       }
     }
